0c41573f0795abbde08dc2f7a68d8bfad00030a6
Emi Zhu <385115337@qq.com>
2022-10-24 22:50:38 +1100

    fi

--- src/components/Expenses/ExpensesChart.jsx
+++ src/components/Expenses/ExpensesChart.jsx
@@ -16,7 +16,10 @@ const ExpensesChart = (props) => {
     { label: "Nov", value: 0 },
     { label: "Dec", value: 0 },
   ];
-  for (const expense of props.expenses)
-    return <Chart dataPoints={chartDataPoints} />;
+  for (const expense of props.expenses) {
+    const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
+    chartDataPoints[expenseMonth].value += expense.amount;
+  }
+  return <Chart dataPoints={chartDataPoints} />;
 };
 export default ExpensesChart;
